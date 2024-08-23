739123140097bd2638f3e54d31e45d9bb805f15e
JanZurb <91033413+JanZurb@users.noreply.github.com>
2024-08-24 01:48:38 +0200

    - get update and delete on donation model

--- backend/controllers/donationController.js
+++ backend/controllers/donationController.js
@@ -2,15 +2,14 @@ const Donation = require('../models/donationModel')
 const mongoose = require('mongoose')
 // get all workouts
 const getDonations = async (req,res) => {
-    const user_id = req.user._id
-
-    const donations = await Donation.find({user_id}).sort({createdAt: -1})
+    const donations = await Donation.find().sort({createdAt: -1})
 
     res.status(200).json(donations)
 }
-// get a single workout
+// get a single donation
 const getDonation = async (req,res) => {
     const {id} = req.params
+    console.log(id)
     if (!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error: ' No such donation'})
     }
@@ -22,38 +21,45 @@ const getDonation = async (req,res) => {
     }
     res.status(200).json(donation)
 }
-// create new workout
+// create new donation
 const createDonation = async (req, res) => {
-    const {title, load, reps} = req.body
-
+    const {victim, task, drink,perpetrator, contactInfo, taskState} = req.body
     let emptyFields = []
 
-    if(!title) {
-        emptyFields.push('title')
+    if(!victim) {
+        emptyFields.push('victim')
     }
-    if(!load) {
-        emptyFields.push('load')
+    if(!task) {
+        emptyFields.push('task')
     }
-    if(!reps) {
-        emptyFields.push('reps')
+    if(!drink) {
+        emptyFields.push('drink')
+    }
+    if(!perpetrator) {
+        emptyFields.push('perpetrator')
+    }
+    if(!contactInfo) {
+        emptyFields.push('contactInfo')
+    }
+    if(!taskState) {
+        emptyFields.push('taskState')
     }
     if(emptyFields.length > 0){
         return res.status(400).json({ error: "Please fill in all the fields", emptyFields })
     }
     //add doc to db
     try {
-        const user_id = req.user._id
-        const donation = await Donation.create({title, load, reps, user_id})
+        const donation = await  Donation.create({victim, task, drink, perpetrator, contactInfo, taskState});
         res.status(200).json(donation)
     } catch (error) {
         res.status(400).json({error: error.message})
     }
 }
 
-// delete a workout
+// delete a donation
 const deleteDonation = async (req,res) => {
-    const {id} = req.params
-
+    const id = req.params.id
+    const {victim, task, drink,perpetrator, contactInfo, taskState} = req.body
     if (!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error: ' No such donation'})
     }
@@ -65,18 +71,24 @@ const deleteDonation = async (req,res) => {
     }
     res.status(200).json(donation)
 }
-// update a workout
-
+// update a donation
 const updateDonation = async (req,res) =>{
-    const {id} = req.params
+    const id = req.params.id
+    const {victim, task, drink,perpetrator, contactInfo, taskState} = req.body
+
 
     if (!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error: ' No such donation'})
     }
 
+
     const donation = await Donation.findOneAndUpdate({_id: id}, {
-        ...req.body
+        victim, task, drink, perpetrator, contactInfo, taskState
     })
+
+    if(taskState == 'inProgress'){
+        //TODO versand per slack odr sms
+        }
 
     if (!donation) {
         return res.status(404).json({error: 'No such donation'})
--- backend/models/donationModel.js
+++ backend/models/donationModel.js
@@ -3,12 +3,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const donationSchema = new Schema({
-    id: {
-        type: Number,
-        required: true
-    },
+
     victim: {
-        type: Number,
+        type: String,
         required: true
     },
     task: {
